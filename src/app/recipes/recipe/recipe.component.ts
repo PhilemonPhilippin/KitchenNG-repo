74a8db86341a376d6225d9143bbf43f351d1c531
Philemon Philippin <p_philippin@hotmail.com>
2023-06-21 18:10:38 +0200

    removed useless code

--- src/app/recipes/recipe/recipe.component.ts
+++ src/app/recipes/recipe/recipe.component.ts
@@ -1,55 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { RecipeService } from './recipe.service';
-import { RecipeIngredientService } from '../recipe-ingredients/recipe-ingredient.service';
-import { IRecipe } from './recipe';
-import { IRecipeIngredient } from '../recipe-ingredients/recipe-ingredient';
-import { PreparationStepService } from '../preparation-steps/preparation-step.service';
-import { IPreparationStep } from '../preparation-steps/preparation-step';
+import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './recipe.component.html',
 })
-export class RecipeComponent implements OnInit {
-  recipe: IRecipe | undefined;
-  recipeIngredients: IRecipeIngredient[] = [];
-  preparationSteps: IPreparationStep[] = [];
-  errorMessages: string[] = [];
-
-  constructor(
-    private route: ActivatedRoute,
-    private recipeService: RecipeService,
-    private recipeIngredientService: RecipeIngredientService,
-    private preparationStepService: PreparationStepService
-  ) {}
-
-  ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.getRecipe(id);
-      this.getRecipeIngredients(id);
-      this.getPreparationSteps(id);
-    }
-  }
-
-  getRecipe(id: string): void {
-    this.recipeService.getRecipe(id).subscribe({
-      next: (recipe) => (this.recipe = recipe),
-      error: (err) => this.errorMessages.push(err),
-    });
-  }
-
-  getRecipeIngredients(recipeId: string): void {
-    this.recipeIngredientService.getRecipeIngredients(recipeId).subscribe({
-      next: (recipeIngredients) => (this.recipeIngredients = recipeIngredients),
-      error: (err) => this.errorMessages.push(err),
-    });
-  }
-
-  getPreparationSteps(recipeId: string): void {
-    this.preparationStepService.getPreparationSteps(recipeId).subscribe({
-      next: (preparationSteps) => (this.preparationSteps = preparationSteps),
-      error: (err) => this.errorMessages.push(err),
-    });
-  }
-}
+export class RecipeComponent {}
